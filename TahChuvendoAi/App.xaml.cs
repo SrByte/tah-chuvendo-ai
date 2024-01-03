74d0f9328ed11689178130fcd1eda2b4c4f6910f
Leonardo Silva <leonardo.silva@linqcode.com.br>
2024-01-03 20:28:52 -0300

    Adicionar arquivos de projeto.

---
+++ TahChuvendoAi/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TahChuvendoAi
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TahChuvendoAi/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TahChuvendoAi
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
